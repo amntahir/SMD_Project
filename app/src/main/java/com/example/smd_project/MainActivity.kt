4c4784d1706528a88eaf5920a3b1bc29be83793e
Maria Zahid <153418292+merrymystical@users.noreply.github.com>
2025-12-08 00:34:46 +0500

    Modify splash screen duration and add sound effect
    Updated splash screen duration and added media player functionality.

--- app/src/main/java/com/example/smd_project/MainActivity.kt
+++ app/src/main/java/com/example/smd_project/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.example.smd_project
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // your splash layout
+        setContentView(R.layout.activity_main)
 
-        // Splash for 5 seconds, then go to Choose Login
+        val logo = findViewById<ImageView>(R.id.appLogo)
+
+        val zoomAnim = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
+        logo.startAnimation(zoomAnim)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.logosound)
+        mediaPlayer?.start()
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@MainActivity, ChooseLoginActivity::class.java))
-            finish() // prevent going back to splash
-        }, 5000)
+            finish()
+        }, 3500)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
